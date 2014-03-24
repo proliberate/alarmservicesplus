c6a65a3720506027031bfec1f2a4e6cf4c47975c
proliberate <andrew@proliberate.org>
2014-03-23 23:59:20 -0400

    Empty rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Alarmservicesplus::Application.config.secret_key_base = 'c11c0d7a899f151e2aef8292246ff19a82ae6c0d21a943164acf1cc56fb4eea367c0c443877713f6245281122d40e9ed85a2a95eff9fd36e39af3b4d61451d22'
